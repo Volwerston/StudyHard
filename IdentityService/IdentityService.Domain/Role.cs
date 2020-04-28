fb47b386fd8d8f4e14673b82cc2599cef79ea555
Yurii Stetskyi <r502g@intra.coop>
2020-04-29 00:41:39 +0300

    Add IdentityService

---
+++ IdentityService/IdentityService.Domain/Role.cs
@@ -0,0 +1,13 @@
+﻿namespace IdentityService.Domain
+{
+    public static class Roles
+    {
+        public static string Regular = "Regular";
+        public static string Tutor = "Tutor";
+    }
+
+    public class Role
+    {
+        public string Name { get; set; }
+    }
+}
