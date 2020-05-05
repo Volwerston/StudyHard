52f1b0d8c4fbcddfca4f2cf6456633fa73e171a4
DawnSpark <uageneral@gmail.com>
2020-05-05 22:39:17 +0300

    Added StudyHard.Website project and sample contoller

---
+++ IdentityService/StudyHard/StudyHard/Controllers/CourseController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityService.Persistence.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using StudyHard.Models;
+using StudyHard.Persistence.Interfaces;
+
+namespace StudyHard.Controllers
+{
+    //for demonstration purposes only
+    
+    public class CourseController : Controller
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseController(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        [HttpGet]
+        [Route("/course")]
+        public async Task<ViewResult> Index()
+        {
+            //we may move it to some service, but if the logic is not complex - nothing bad to keep it in controller
+            var courses = _courseRepository.GetCourses();
+            CourseListModel model = new CourseListModel
+            {
+                Courses = await _courseRepository.GetCourses()
+            };
+            return View(model);
+        }
+        
+        [Route("/course/{id}/api")]
+        [HttpGet]
+        public async Task<ActionResult> GetCourseInfo(int id)
+        {
+            //here we just display the data, so I didn't create a model
+            var course = await _courseRepository.GetCourseById(id);
+            return new JsonResult(course);
+        }
+        
+    }
+}
---
+++ IdentityService/StudyHard/StudyHard/Models/CourseListModel.cs
@@ -0,0 +1,15 @@
+﻿using IdentityService.Domain;
+using StudyHard.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyHard.Models
+{
+    //Set as an example, usually models will be quite different from the entities
+    public class CourseListModel
+    {
+        public List<Course> Courses { get; set; }
+    }
+}
