893e8d88b510516c979c0b263032c5ac1a0e51b9
Yurii Stetskyi <r502g@intra.coop>
2020-05-06 01:39:19 +0300

    Added console app for running db migrations

---
+++ IdentityService/ExecuteMigrations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DbUp;
+
+namespace ExecuteMigrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // IMPORTANT: before running migration with new scripts,
+            // make sure you set 'Copy to Output Directory' as 'Copy always' for new .sql files
+
+            const string connectionString = "SpecifyBeforeRunning";
+
+            var upgradeEngine = DeployChanges.To
+                .SqlDatabase(connectionString)
+                .WithScriptsFromFileSystem("migrations")
+                .LogToConsole()
+                .Build();
+
+            var result = upgradeEngine.PerformUpgrade();
+            if (!result.Successful)
+            {
+                Console.WriteLine(result.Error);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
