5ce7937d294004b7748edf4981c2b762c87e8587
Yurii Stetskyi <r502g@intra.coop>
2020-05-20 21:21:43 +0300

    Add test customer and tutor with OAuth support

---
+++ IdentityService/ExecuteMigrations/migrations/0009_Add test tutor and customer with Oauth support.sql
@@ -0,0 +1,14 @@
+﻿-- Users
+
+INSERT INTO [dbo].[User] VALUES('studyhardcustomer@gmail.com', 'Test Customer', 1, '1970-06-12')
+INSERT INTO [dbo].[User] VALUES('studyhardtutor@gmail.com', 'Test Tutor', 0, '1987-08-25')
+
+-- User Roles
+INSERT INTO [dbo].[UserRoles] VALUES(11, 1)
+INSERT INTO [dbo].[UserRoles] VALUES(12, 1)
+INSERT INTO [dbo].[UserRoles] VALUES(12, 2)
+
+-- User Skills
+INSERT INTO [dbo].[UserSkills] VALUES(12, 1)
+INSERT INTO [dbo].[UserSkills] VALUES(12, 2)
+INSERT INTO [dbo].[UserSkills] VALUES(12, 3)
