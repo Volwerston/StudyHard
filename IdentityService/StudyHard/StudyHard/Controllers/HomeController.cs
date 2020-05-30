3adf1141819c2dc82a62926644c374e41d5abc3a
DawnSpark <uageneral@gmail.com>
2020-05-30 13:29:48 +0300

    Created home model and extended methods for home controller

--- IdentityService/StudyHard/StudyHard/Controllers/HomeController.cs
+++ IdentityService/StudyHard/StudyHard/Controllers/HomeController.cs
@@ -1,23 +1,63 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityService.Domain;
+using IdentityService.Persistence.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using StudyHard.Helpers;
 using StudyHard.Models;
 
 namespace StudyHard.Controllers
 {
     [AllowAnonymous]
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUserRepository _userRepository;
+        public HomeController(ILogger<HomeController> logger, IUserRepository userRepository, IUserInfoProvider userInfoProvider)
+            : base(userRepository, userInfoProvider)
         {
             _logger = logger;
+            _userRepository = userRepository;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            long userId = GetUserId();
+            var user = _userRepository.FindUsers(new List<long> { userId }).FirstOrDefault(); //TODO: add a method for single user, maybe?
+            var roles = (await _userRepository.FindRoles(user)).ToList(); //There should be only one role, ain't it? makes UI a bit more complicated
+            var model = new HomeModel
+            {
+                BirthDate = user?.BirthDate,
+                Name = user?.Name,
+                Email = user?.Email,
+                Gender = user?.Gender,
+                Roles = roles,
+                UserId = userId
+            };
+            return View(model);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser([FromBody] HomeModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                //Better to use dto for this
+                var user = new User
+                {
+                    Id = form.UserId,
+                    Name = form.Name,
+                    Gender = form.Gender ?? 0,
+                    BirthDate = form.BirthDate
+                };
+                await _userRepository.Update(user);
+                    
+            }
+            return RedirectPermanent("/Home/Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ IdentityService/StudyHard/StudyHard/Models/HomeModel.cs
@@ -0,0 +1,20 @@
+﻿using IdentityService.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyHard.Models
+{
+    public class HomeModel
+    {
+        public long UserId { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public List<Role> Roles { get; set; }
+        public long? Gender { get; set; }
+        public DateTime? BirthDate { get; set; }
+        //TODO: add Interests, upload photo
+
+    }
+}
